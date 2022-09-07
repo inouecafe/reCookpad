e3badda7f7131ff88ac53d394167c3cf62f4069c
{YuitoFukunaga} <{YuitoFukunaga1122@gmail.com}>
2022-09-07 15:40:44 +0900

    formが使えるようになったはず

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask_wtf import CSRFProtect
+
+csrf = CSRFProtect()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from.mapping(
+        SECRET_KEY="AKJNAJKAKJSFJMF<JFI"
+    )
+    csrf.init_app(app)
     from apps.recookpadSite import views
     app.register_blueprint(views.recookpadSite, url_prefix="/")
     return app
